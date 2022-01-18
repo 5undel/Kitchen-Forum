69253108e4fb3cee31678a29fa8535d9029de87a
5undel <max.sundel@Choice.se>
2022-01-18 16:58:32 +0000

    Add room.topic to home.html

--- base/models.py
+++ base/models.py
@@ -10,8 +10,8 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    #host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    #topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     #participants = models.ManyToManyField(User, related_name='participants', blank=True)
