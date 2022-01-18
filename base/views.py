b578de26f40dffcf8a6cbec5910343d585a5c5ff
5undel <max.sundel@Choice.se>
2022-01-18 17:45:13 +0000

    Add delete method

--- base/views.py
+++ base/views.py
@@ -49,3 +49,15 @@ def updateRoom(request, pk):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You do not have permission to delet this!!')
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'object': room})
