908f6a118888b6f51458b9b7619f8c869b1657b3
5undel <max.sundel@Choice.se>
2022-01-18 16:17:32 +0000

    Move home.html to new temp

--- base/views.py
+++ base/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+rooms = [
+    {'id':1, 'name':'cook with me'},
+    {'id':2, 'name':'recept'},
+    {'id':3, 'name':'chefs'},
+]
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
