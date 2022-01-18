59e12d92718a25c6057d284c1c0e613f8de4f5c9
5undel <max.sundel@Choice.se>
2022-01-18 17:58:42 +0000

    Add search request

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
@@ -11,8 +11,11 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    r = request.GET.get('r') if request.GET.get('r') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=r)
+
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
