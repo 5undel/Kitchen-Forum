a4ade0a4ea0be8782012f1dd8afdcc437989fe40
5undel <max.sundel@Choice.se>
2022-01-19 07:36:03 +0000

    Add deleteMessage method

--- base/views.py
+++ base/views.py
@@ -132,3 +132,17 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'object': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You do not have permission to delet this!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'object': message})
