feeb6724767baeb62af2491ae104171574b2bf90
5undel <max.sundel@Choice.se>
2022-01-19 07:48:06 +0000

    Add user-page to urls

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('mypage/<str:pk>/', views.userPage, name="user-page"),
     
     path('create-room', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
